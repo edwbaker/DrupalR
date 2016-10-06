6c3673e3237ab6a03f9f6eafee7f4ecb391f2480
Ed Baker <edwbaker@gmail.com>
2016-10-06 15:03:52 +0100

    Update drupal.R

--- R/drupal.R
+++ R/drupal.R
@@ -18,7 +18,7 @@ drupalr.logout <- function(d_url, c) {
   getURL(paste(d_url,"user/logout"), curl = c)
 }
 
-drupalr.get <- function(d_url, path, c) {
+drupalr.get <- function(d_url, path, c = getCurlHandle()) {
   return(getURL(paste0(d_url,path), curl = c))
 }
 
