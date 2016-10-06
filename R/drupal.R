14406d2bc1962183bcca7b461aa93979908ce57f
Ed Baker <edwbaker@gmail.com>
2016-10-06 14:20:04 +0100

    Use RCurl

--- R/drupal.R
+++ R/drupal.R
@@ -1,19 +1,22 @@
-library(httr);
+library(RCurl);
 
 drupalr.authenticate <- function(d_url, d_name, d_pass) {
-  post <- POST(paste0(d_url, "/user/login"), body = list(name = d_name, pass = d_pass, form_id = "user_login", op="Log in"));
-  h <- handle(d_url);
-  return (h);
+  pars=list(
+    op="Log in",
+    name = d_name,
+    pass= d_pass,
+    form_id="user_login"
+  )
+  curl = getCurlHandle()
+  curlSetOpt(cookiejar="cookies.txt",  useragent="DrupalR", followlocation = TRUE, curl=curl)
+  html = postForm(paste(paste0(d_url,"/user/login")), .params = pars, curl=curl)
+  return(curl)
 }
 
-drupalr.logout <- function(h) {
-  GET(paste0(h['url'], "/user/logout"));
+drupalr.logout <- function(d_url, c) {
+  getURL(paste(d_url,"user/logout"), curl = c)
 }
 
 drupalr.get <- function(h, path, userAgent = NULL) {
-  if (is.null(userAgent)) {
-    userAgent <- "DrupalR";
-  }
-  get <- GET(paste0(h['url'], "/", path), user_agent = userAgent);
-  return (get);
+  
 }
