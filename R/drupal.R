b7e74c2e92a473dd9935e099733c2f05e2cd7a15
Ed Baker <edwbaker@gmail.com>
2017-08-15 12:57:33 +0100

    Namespace all functions.

--- R/drupal.R
+++ R/drupal.R
@@ -1,5 +1,3 @@
-library(RCurl);
-
 drupalr.authenticate <- function(d_url, d_name, d_pass) {
   pars=list(
     op="Log in",
@@ -7,34 +5,34 @@ drupalr.authenticate <- function(d_url, d_name, d_pass) {
     pass= d_pass,
     form_id="user_login"
   )
-  curl = getCurlHandle()
-  curl = drupalr.agent("DrupalR", curl)
-  curlSetOpt(cookiejar="cookies.txt", followlocation = TRUE, curl=curl)
-  html = postForm(paste(paste0(d_url,"/user/login")), .params = pars, curl=curl)
+  curl = Rcurl::getCurlHandle()
+  curl = DrupalR::drupalr.agent("DrupalR", curl)
+  RCurl::curlSetOpt(cookiejar="cookies.txt", followlocation = TRUE, curl=curl)
+  html = DrupalR::postForm(paste(paste0(d_url,"/user/login")), .params = pars, curl=curl)
   return(curl)
 }
 
 drupalr.logout <- function(d_url, c) {
-  getURL(paste(d_url,"user/logout"), curl = c)
+  DrupalR::getURL(paste(d_url,"user/logout"), curl = c)
 }
 
 drupalr.get <- function(d_url, path, c = getCurlHandle()) {
-  return(getURL(paste0(d_url,path), curl = c))
+  return(DrupalR::getURL(paste0(d_url,path), curl = c))
 }
 
 drupalr.agent <- function(agent, c) {
-  curlSetOpt(useragent=agent, curl = c)
+  RCurl::curlSetOpt(useragent=agent, curl = c)
   return(c)
 }
 
 drupalr.postForm <- function(d_url, d_path, form_id, pars, c, num=1) {
   #First load form to get token and build_id
-  form <- getURL(paste0(d_url, d_path), curl = c);
-  data <- read_html(form)
+  form <- DrupalR::getURL(paste0(d_url, d_path), curl = c);
+  data <- xml2::read_html(form)
   
   if (form_id == "comment_form") { num <- 3}
   
-  data <- xml_find_first(data, paste0('//*[(@id = "',gsub("_", "-", form_id), '")]'));
+  data <- xml2::xml_find_first(data, paste0('//*[(@id = "',gsub("_", "-", form_id), '")]'));
   form_token <- unname(xml_attrs(xml_find_all(data, '//*[(@name = "form_token")]'))[[num]]['value'])
   form_build_id <- unname(xml_attrs(xml_find_all(data, '//*[(@name = "form_build_id")]'))[[num]]['value'])
   form_id <- unname(xml_attrs(xml_find_all(data, '//*[(@name = "form_id")]'))[[num]]['value'])
@@ -47,7 +45,7 @@ drupalr.postForm <- function(d_url, d_path, form_id, pars, c, num=1) {
   
   pars = c(pars, i_pars);
   
-  html = postForm(paste0(d_url, d_path), .params = pars, curl=c)
+  html = DrupalR::postForm(paste0(d_url, d_path), .params = pars, curl=c)
 }
 
 drupalr.postComment <- function(d_url, d_path, body, params, curl) {
@@ -57,5 +55,5 @@ drupalr.postComment <- function(d_url, d_path, body, params, curl) {
     'comment_body[und][0][format]' = 'comment_filtered_html'
   );
   params <- c(params, comment_params);
-  drupalr.postForm(d_url, d_path, "comment_form", params, curl);
+  DrupalR::drupalr.postForm(d_url, d_path, "comment_form", params, curl);
 }
